4609e4133de98c77e0b950670aed67fb856f83f5
Oli Zimpasser <oglimmer@gmail.com>
2023-12-25 16:25:11 +0100

    Change default baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,7 @@
-
-console.log(`process.env.BASE_URL: ${process.env.BASE_URL}`)
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
-    baseURL: process.env.BASE_URL || '/html-video-grab/'
+    baseURL: '/html-video-grab/'
   }
 })
